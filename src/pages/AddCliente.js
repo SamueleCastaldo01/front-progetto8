c84e5f8e1184c03da3855fa9868b759053107282
FedericoIafolla <iafollafederico@gmail.com>
2024-11-13 19:15:50 +0100

    Miglioramento della pagina

--- src/pages/AddCliente.js
+++ src/pages/AddCliente.js
@@ -25,6 +25,8 @@ export function AddCliente() {
     const [cognomeContatto, setCognomeContatto] = useState('');
     const [telefonoContatto, setTelefonoContatto] = useState('');
     const [logoAziendale, setLogoAziendale] = useState('');
+    const [idSedeLegale, setIdSedeLegale] = useState('');
+    const [idSedeOperativa, setIdSedeOperativa] = useState('');
 
     const token = localStorage.getItem('authToken');
 
@@ -44,6 +46,8 @@ export function AddCliente() {
         setCognomeContatto('');
         setTelefonoContatto('');
         setLogoAziendale('');
+        setIdSedeLegale('');
+        setIdSedeOperativa('');
     };
 
     const capitalizeWords = (str) => {
@@ -73,6 +77,8 @@ export function AddCliente() {
             cognomeContatto,
             telefonoContatto,
             logoAziendale,
+            idSedeLegale,
+            idSedeOperativa,
             createdAt: new Date().toISOString(),
 
         };
@@ -137,6 +143,26 @@ export function AddCliente() {
                 <form onSubmit={handleSubmit}>
                     <div className='row'>
                         <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
+                            <TextField className='w-100' required label="Nome Contatto" variant="outlined" color='tertiary' value={nomeContatto}
+                                onChange={(e) => {
+                                    const formattedNome = capitalizeWords(e.target.value);
+                                    setNomeContatto(formattedNome);
+                                }}
+                            />
+                        </div>
+                        <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
+                            <TextField className='w-100' required label="Cognome Contatto" variant="outlined" color='tertiary' value={cognomeContatto}
+                                onChange={(e) => {
+                                    const formattedCognome = capitalizeWords(e.target.value);
+                                    setCognomeContatto(formattedCognome);
+                                }}
+                            />
+                        </div>
+                        <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
+                            <TextField className='w-100' type='number' required label="Numero di Telefono Contatto" variant="outlined" color='tertiary' value={telefonoContatto} onChange={(e) => setTelefonoContatto(e.target.value)} />
+                        </div>
+
+                        <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
                             <TextField className='w-100' required label="Ragione Sociale" variant="outlined" color='tertiary' value={ragioneSociale} onChange={(e) => setRagioneSociale(e.target.value.toLowerCase())} />
                         </div>
                         <div className='d-flex mt-4 col-lg-4 col-md-6 col-sm-12'>
@@ -164,24 +190,12 @@ export function AddCliente() {
                             <TextField className='w-100' label="Email Contatto" variant="outlined" color='tertiary' value={emailContatto} onChange={(e) => setEmailContatto(e.target.value)} />
                         </div>
                         <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
-                            <TextField className='w-100' required label="Nome Contatto" variant="outlined" color='tertiary' value={nomeContatto}
-                                onChange={(e) => {
-                                    const formattedNome = capitalizeWords(e.target.value);
-                                    setNomeContatto(formattedNome);
-                                }}
-                            />
+                            <TextField className='w-100' required label="ID Sede Legale" variant="outlined" color='tertiary' value={idSedeLegale} onChange={(e) => setIdSedeLegale(e.target.value)} />
                         </div>
                         <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
-                            <TextField className='w-100' required label="Cognome Contatto" variant="outlined" color='tertiary' value={cognomeContatto}
-                                onChange={(e) => {
-                                    const formattedCognome = capitalizeWords(e.target.value);
-                                    setCognomeContatto(formattedCognome);
-                                }}
-                            />
+                            <TextField className='w-100' required label="ID Sede Operativa" variant="outlined" color='tertiary' value={idSedeOperativa} onChange={(e) => setIdSedeOperativa(e.target.value)} />
                         </div>
-                        <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
-                            <TextField className='w-100' type='number' required label="Numero di Telefono Contatto" variant="outlined" color='tertiary' value={telefonoContatto} onChange={(e) => setTelefonoContatto(e.target.value)} />
-                        </div>
+
                     </div>
                     <Button className='mt-4' type="submit" variant="contained">Aggiungi Cliente</Button>
                 </form>
