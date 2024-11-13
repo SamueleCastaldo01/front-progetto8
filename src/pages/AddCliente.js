5592c03b221764dc2bd18457650caeae6303376c
SaraPavone <172259730+SaraPavone@users.noreply.github.com>
2024-11-13 17:03:31 +0100

    addCliente

--- src/pages/AddCliente.js
+++ src/pages/AddCliente.js
@@ -3,45 +3,52 @@ import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { FormControl, InputLabel, MenuItem, Select, Collapse, Typography } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'; // Import dropdown icon
-import { db } from '../firebase-config';
-import { collection, addDoc, query, where, getDocs, Timestamp  } from 'firebase/firestore';
-import moment from 'moment';
-import CodiceFiscale from 'codice-fiscale-js';
-import { notifyErrorAddCliente, notifyErrorAddUsername, successAddCliente } from '../components/Notify';
+
 
 export function AddCliente() {
     const navigate = useNavigate();
-    const [gender, setGender] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('12345678');
-    const [nome, setNome] = useState('');
-    const [cognome, setCognome] = useState('');
-    const [dataNascita, setDataNascita] = useState('');
-    const [cittaNascita, setCittaNascita] = useState('');
-    const [provinciaNascita, setProvinciaNascita] = useState('');
-    const [codiceFiscale, setCodiceFiscale] = useState('');
-    const [telefono, setTelefono] = useState('');
-    const [email, setEmail] = useState('');
-    const [showOptionalFields, setShowOptionalFields] = useState(false); // State for optional fields
 
-    const handleGenderChange = (event) => {
-        setGender(event.target.value);
-    };
+    const [ragioneSociale, setRagioneSociale] = useState('');
+    const [partitaIva, setPartitaIva] = useState('');
+    const [email, setEmail] = useState('');
+    const [dataInserimento, setDataInserimento] = useState('');
+    const [dataUltimoContatto, setDataUltimoContatto] = useState('');
+    const [fattutatoAnnuale, setFattutatoAnnuale] = useState('');
+    const [pec, setPec] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [emailContatto, setEmailContatto] = useState('');
+    const [nomeContatto, setNomeContatto] = useState('');
+    const [cognomeContatto, setCognomeContatto] = useState('');
+    const [telefonoContatto, setTelefonoContatto] = useState('');
+    const [logoAziendale, setLogoAziendale] = useState('');
+
+
+   //const [gender, setGender] = useState('');
+   //const [username, setUsername] = useState('');
+   //const [password, setPassword] = useState('12345678');
+   //const [dataNascita, setDataNascita] = useState('');
+   //const [cittaNascita, setCittaNascita] = useState('');
+   //const [provinciaNascita, setProvinciaNascita] = useState('');
+   //const [showOptionalFields, setShowOptionalFields] = useState(false); // State for optional fields
+
+  //  const handleGenderChange = (event) => {
+  //      setGender(event.target.value);
+  //};
 
     const handleReset = () => {
-        setUsername("");
-        setPassword("");
-        setNome("");
-        setCognome("");
-        setGender("");
-        setDataNascita("");
-        setCittaNascita("");
-        setProvinciaNascita("");
-        setCodiceFiscale("");
-        setTelefono("");
-        setEmail("");
+        setRagioneSociale('');
+        setPartitaIva('');
+        setEmail('');
+        setDataInserimento('');
+        setDataUltimoContatto('');
+        setFattutatoAnnuale('');
+        setPec('');
+        setTelefono('');
+        setEmailContatto('');
+        setNomeContatto('');
+        setCognomeContatto('');
+        setTelefonoContatto('');
+        setLogoAziendale('');
     };
 
     const capitalizeWords = (str) => {
@@ -58,24 +65,24 @@ export function AddCliente() {
     };
 
 
-    const handleGeneratePassword = () => {
-        const length = 10;
-        const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        const upperCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        const numberCharset = "0123456789";
+  //  const handleGeneratePassword = () => {
+  //     const length = 10;
+  //     const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  //     const upperCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //     const numberCharset = "0123456789";
 
-        let password = "";
-        password += upperCharset.charAt(Math.floor(Math.random() * upperCharset.length));
-        password += numberCharset.charAt(Math.floor(Math.random() * numberCharset.length));
+  //     let password = "";
+  //     password += upperCharset.charAt(Math.floor(Math.random() * upperCharset.length));
+  //     password += numberCharset.charAt(Math.floor(Math.random() * numberCharset.length));
 
-        for (let i = 2; i < length; i++) {
-            password += charset.charAt(Math.floor(Math.random() * charset.length));
-        }
+  //     for (let i = 2; i < length; i++) {
+  //         password += charset.charAt(Math.floor(Math.random() * charset.length));
+  //     }
 
-        password = password.split('').sort(() => Math.random() - 0.5).join('');
-        
-        setPassword(password);
-    };
+  //     password = password.split('').sort(() => Math.random() - 0.5).join('');
+  //     
+  //     setPassword(password);
+  // };
 
     return (
         <motion.div
@@ -89,74 +96,48 @@ export function AddCliente() {
                 <form onSubmit={handleSubmit}>
                     <div className='row'>
                         <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
-                            <TextField className='w-100' required label="Username" variant="outlined" color='tertiary' value={username} onChange={(e) => setUsername(e.target.value.toLowerCase())} />
+                            <TextField className='w-100' required label="Ragione Sociale" variant="outlined" color='tertiary' value={ragioneSociale} onChange={(e) => setRagioneSociale(e.target.value.toLowerCase())} />
                         </div>
                         <div className='d-flex mt-4 col-lg-4 col-md-6 col-sm-12'>
-                            <TextField className='w-100' required label="Password" variant="outlined" color='tertiary' value={password} onChange={(e) => setPassword(e.target.value)} />
-                            <Button onClick={handleGeneratePassword} variant="contained">Genera</Button>    
+                            <TextField className='w-100' required label="Partita IVA" variant="outlined" color='tertiary' value={partitaIva} onChange={(e) => setPartitaIva(e.target.value)} />   
+                        </div>
+                        <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
+                                        <TextField className='w-100' label="Email" variant="outlined" color='tertiary' value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
+                                        <TextField className='w-100' type='date' label="Data di inserimento" variant="outlined" color='tertiary' value={dataInserimento} onChange={(e) => setDataInserimento(e.target.value)} InputLabelProps={{ shrink: true }} />
+                        </div>
+                        <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
+                                        <TextField className='w-100' type='date' label="Data ultimo contatto" variant="outlined" color='tertiary' value={dataUltimoContatto} onChange={(e) => setDataUltimoContatto(e.target.value)} InputLabelProps={{ shrink: true }} />
+                        </div>
+                        <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
+                            <TextField className='w-100' type='number' required label="Fattutato Annuale" variant="outlined" color='tertiary' value={fattutatoAnnuale} onChange={(e) => setFattutatoAnnuale(e.target.value)} />
+                        </div>
+                        <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
+                                        <TextField className='w-100' label="Pec" variant="outlined" color='tertiary' value={pec} onChange={(e) => setPec(e.target.value)} />
                         </div>
                         <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
                             <TextField className='w-100' type='number' required label="Numero di Telefono" variant="outlined" color='tertiary' value={telefono} onChange={(e) => setTelefono(e.target.value)} />
                         </div>
                         <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
-                            <TextField className='w-100' required label="Nome" variant="outlined" color='tertiary' value={nome}   
+                                        <TextField className='w-100' label="Email Contatto" variant="outlined" color='tertiary' value={emailContatto} onChange={(e) => setEmailContatto(e.target.value)} />
+                        </div>
+                        <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
+                            <TextField className='w-100' required label="Nome Contatto" variant="outlined" color='tertiary' value={nomeContatto}   
                                 onChange={(e) => {
                                 const formattedNome = capitalizeWords(e.target.value); 
-                                setNome(formattedNome); }}  
+                                setNomeContatto(formattedNome); }}  
                             />
                         </div>
                         <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
-                            <TextField className='w-100' required label="Cognome" variant="outlined" color='tertiary' value={cognome} 
+                            <TextField className='w-100' required label="Cognome Contatto" variant="outlined" color='tertiary' value={cognomeContatto} 
                                 onChange={(e) => {
                                 const formattedCognome = capitalizeWords(e.target.value); 
-                                setCognome(formattedCognome); }}   
+                                setCognomeContatto(formattedCognome); }}   
                             />
                         </div>
-
-                        {/* Optional Fields Section */}
-                        <div className='mt-4 col-lg-12'>
-                            <Typography 
-                                variant="h6" 
-                                onClick={() => setShowOptionalFields(!showOptionalFields)} 
-                                style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}
-                            >
-                                Campi Facoltativi 
-                                {showOptionalFields ? 
-                                    <ExpandMoreIcon style={{ marginLeft: '8px', transform: 'rotate(180deg)' }} /> : 
-                                    <ExpandMoreIcon style={{ marginLeft: '8px' }} />
-                                }
-                            </Typography>
-                            <Collapse in={showOptionalFields}>
-                                <div className='row'>
-                                    <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
-                                        <FormControl fullWidth color='tertiary'>
-                                            <InputLabel id="gender-select-label">Genere</InputLabel>
-                                            <Select
-                                                labelId="gender-select-label"
-                                                id="gender-select"
-                                                value={gender}
-                                                label="Genere"
-                                                onChange={handleGenderChange}
-                                            >
-                                                <MenuItem value="maschio">Maschio</MenuItem>
-                                                <MenuItem value="femmina">Femmina</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                    </div>
-                                    <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
-                                        <TextField className='w-100' type='date' label="Data di nascita" variant="outlined" color='tertiary' value={dataNascita} onChange={(e) => setDataNascita(e.target.value)} InputLabelProps={{ shrink: true }} />
-                                    </div>
-                                    <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
-                                        <TextField className='w-100' label="Città di nascita" variant="outlined" color='tertiary' value={cittaNascita} onChange={(e) => setCittaNascita(e.target.value)} />
-                                    </div>
-                                    <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
-                                        <TextField className='w-100' label="Provincia di nascita" variant="outlined" color='tertiary' value={provinciaNascita} onChange={(e) => setProvinciaNascita(e.target.value)} />
-                                    </div>
-                                    <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
-                                        <TextField className='w-100' label="Email" variant="outlined" color='tertiary' value={email} onChange={(e) => setEmail(e.target.value)} />
-                                    </div>
-                                </div>
-                            </Collapse>
+                        <div className='mt-4 col-lg-4 col-md-6 col-sm-12'>
+                            <TextField className='w-100' type='number' required label="Numero di Telefono Contatto" variant="outlined" color='tertiary' value={telefonoContatto} onChange={(e) => setTelefonoContatto(e.target.value)} />
                         </div>
                     </div>
                     <Button className='mt-4' type="submit" variant="contained">Aggiungi Cliente</Button>
