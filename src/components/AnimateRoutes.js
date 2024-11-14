086a9fd2b18d45b0b71fa1092a0bc30aeb5a3711
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-11-14 14:22:43 +0100

    commit add stato Fattura

--- src/components/AnimateRoutes.js
+++ src/components/AnimateRoutes.js
@@ -15,6 +15,7 @@ import { AddCliente } from '../pages/AddCliente';
 import Register from '../pages/Register';
 import { AddFatture } from '../pages/AddFatture';
 import { FattureList } from '../pages/FattureList';
+import { AddStatoFattura } from '../pages/AddStatoFattura';
 
 
 
@@ -49,6 +50,7 @@ return (
       <Route path="/dashboardcustomer/:id" element={<DashboardCustomer />} /> 
       <Route path="/listafatture" element={<FattureList />} />
       <Route path="/aggiungifatture" element={<AddFatture />} /> 
+      <Route path="/aggiungistatofattura" element={<AddStatoFattura />} /> 
 
 
     </Route>
